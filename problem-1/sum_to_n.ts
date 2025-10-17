29b7e3b2831104d3544630267bfcd561e32edb50
thuongpx12 <phungxuanthuong@gmail.com>
2025-10-17 10:26:04 +0700

    create folder and resolve problems

---
+++ problem-1/sum_to_n.ts
@@ -0,0 +1,17 @@
+var sum_to_n_a = function (n: number) {
+  let sum = 0;
+  if (n === 0) return 0;
+  for (let i = 0; i <= n; i++) {
+    sum = sum + i;
+  }
+  return sum;
+};
+
+var sum_to_n_b = function (n: number) {
+  let sum = 0;
+  if (n === 0) return 0;
+  sum = (n * (n + 1)) / 2;
+  return sum;
+};
+
+var sum_to_n_c = function (n: number) {};
